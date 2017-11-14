9b1d111d8788a44ea067f8e467752f56e2642129
Josh S <josh@joshsomma.com>
2017-11-14 16:22:27 +0000

    first session w Dom Patmore, built a class w test cases

---
+++ test_hello.py
@@ -0,0 +1,23 @@
+# import libs
+import unittest
+
+class HelloTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.greeting = 'Hello World!'
+
+    def reverseString(self,msg):
+        return msg[::-1]
+
+    def testIhaveAVar(self):
+        self.assertIsNotNone(self.greeting)
+        self.assertTrue(self.greeting=='Hello World!', msg='This should say "Hello World!"')
+        self.assertTrue('Hello' in self.greeting)
+
+    def testThatICanReverseTheGreeting(self):
+        self.assertTrue(self.greeting=='Hello World!', msg='This should say "Hello World!"')
+        self.assertTrue(self.reverseString(self.greeting) == '!dlroW olleH')
+
+
+# d = HelloTestCase()
+# d.reverseString("hello")
